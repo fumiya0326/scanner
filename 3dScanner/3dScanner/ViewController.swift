1023a68cf3aebe3104634e74f051f162f184a376
fumiya <buchi0326iwa@gmail.com>
2023-02-22 22:11:26 +0900

    first commit

---
+++ 3dScanner/3dScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3dScanner
+//
+//  Created by ISN98 on 2023/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
